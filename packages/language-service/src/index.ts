e3a9a926843f7a00d4dcfa419bc4a5a470129ab3
Vida Xie <vida_2020@163.com>
2026-03-31 17:57:50 +0800

    feat: migrate completion to language-service (#93)
    * feat: migrate completion to language-service
    
    * remove useless files
    
    * rename

--- packages/language-service/src/index.ts
+++ packages/language-service/src/index.ts
@@ -3,11 +3,13 @@ import type { IWorkspaceState } from './types'
 import { create as createNpmxCatalogService } from './plugins/catalog'
 import { create as createNpmxDiagnosticsService } from './plugins/diagnostics'
 import { create as createNpmxHoverService } from './plugins/hover'
+import { create as createNpmxVersionCompletionService } from './plugins/version-completion'
 
 export function createNpmxLanguageServicePlugins(workspace: IWorkspaceState): LanguageServicePlugin[] {
   return [
     createNpmxCatalogService(workspace),
     createNpmxDiagnosticsService(workspace),
     createNpmxHoverService(workspace),
+    createNpmxVersionCompletionService(workspace),
   ]
 }
